ee7290a5301d6cf1ab361f8de0d5a46159e93f65
akhileshgoud <akhilesh.gounikadi@xcubelabs.com>
2022-12-12 20:48:37 +0530

    [TASK]: adding react app with pre defined testcase

---
+++ react-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello React
+        </a>
+    </div>
+  );
+}
+
+export default App;
